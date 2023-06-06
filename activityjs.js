f0fbd33a3e26ee21741b08a328aecd62b8189430
Cris Vincent Ferrer <crisvinf@gmail.com>
2023-06-06 20:04:07 +0800

    initial  commit

---
+++ activityjs.js
@@ -0,0 +1,15 @@
+function relationship(stat){
+    let res="Absent";
+    // TODO: need to refactor. xchange the order of condition
+    if(("minahal" === stat) || ("nasaktan"===stat) || ("nagmahal"===stat)){ //// TODO: need to refactor this code. Exchange order of condition
+        res="Present";
+    }
+   return res;
+}
+
+console.log(relationship("minahal"));
+console.log(relationship("nasaktan"));
+console.log(relationship("nagmahal"));
+console.log(relationship("pinili"));
+
+
